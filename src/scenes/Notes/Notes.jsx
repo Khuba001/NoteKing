79e3f87250db00b0d6c1241131c71cdceb7b07d5
Khuba001 <jakublewinski2701@gmail.com>
2024-12-11 23:53:20 +0100

    func: different sorting options

--- src/scenes/Notes/Notes.jsx
+++ src/scenes/Notes/Notes.jsx
@@ -20,10 +20,21 @@ import { useNavigate } from "react-router-dom";
 function Notes({ notesData, setNotesData }) {
   const navigate = useNavigate();
   const [query, setQuery] = useState("");
+  const [sortBy, setSortBy] = useState("");
+  const [currentPage, setCurrentPage] = useState(1);
+  const itemsPerPage = 4;
 
   const filteredNotes = notesData.filter((note) =>
     note.title.toLowerCase().includes(query.toLowerCase())
   );
+
+  const sortedItems = [...filteredNotes].sort((a, b) => {
+    if (sortBy === "normal") return filteredNotes;
+    if (sortBy === "favourites") return b.isImportant - a.isImportant;
+    if (sortBy === "longest") return b.title.length - a.title.length;
+    return 0;
+  });
+
   function handleDeleteNote(id) {
     setNotesData((notesData) =>
       notesData.map((note) =>
@@ -41,6 +52,11 @@ function Notes({ notesData, setNotesData }) {
   function handleNavigate(id) {
     navigate(`/notes/${id}`);
   }
+  function handleSortBy(e) {
+    const sortOption = e.target.value;
+    setSortBy(sortOption);
+  }
+  function handlePageChange(val) {}
 
   return (
     <Box display="flex" flexDirection="column">
@@ -73,10 +89,11 @@ function Notes({ notesData, setNotesData }) {
                 label="Sort by"
                 variant="filled"
                 name="sort"
-                value="normal"
+                value={sortBy}
+                onChange={handleSortBy}
               >
                 <MenuItem value="normal">Normal</MenuItem>
-                <MenuItem value="favouites">Favourites</MenuItem>
+                <MenuItem value="favourites">Favourites</MenuItem>
                 <MenuItem value="longest">Longest</MenuItem>
               </Select>
             </FormControl>
@@ -99,7 +116,7 @@ function Notes({ notesData, setNotesData }) {
           justifyItems="center"
           alignContent="center"
         >
-          {filteredNotes.map(
+          {sortedItems.map(
             (item) =>
               !item.isTrashed && (
                 <NoteItem
@@ -117,7 +134,13 @@ function Notes({ notesData, setNotesData }) {
           )}{" "}
         </Box>
       </Box>
-      <Pagination siblingCount={0} count={4} sx={{ alignSelf: "center" }} />
+      <Pagination
+        siblingCount={0}
+        count={Math.ceil(sortedItems.length / itemsPerPage)}
+        sx={{ alignSelf: "center" }}
+        page={currentPage}
+        onChange={handlePageChange}
+      />
     </Box>
   );
 }
