45c52e76c522394b52b80109516065f34014bb43
Khuba001 <jakublewinski2701@gmail.com>
2024-12-24 01:38:48 +0100

    edit component and closing edit screen

--- src/scenes/SingleNote/Note.jsx
+++ src/scenes/SingleNote/Note.jsx
@@ -1,4 +1,15 @@
-import { Box, colors, Typography, TextField, Button } from "@mui/material";
+import {
+  Box,
+  colors,
+  Typography,
+  TextField,
+  Button,
+  Checkbox,
+  MenuItem,
+  FormControl,
+  InputLabel,
+  Select,
+} from "@mui/material";
 import { tokens, useMode } from "../../theme";
 import Header from "../../components/Header/Header";
 import NoteItem from "../../components/NoteItem/NoteItem";
@@ -7,6 +18,7 @@ import { useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { Formik } from "formik";
 import * as yup from "yup";
+import { Favorite, FavoriteBorder } from "@mui/icons-material";
 
 function NoteDetails({ currentNote }) {
   const theme = useTheme();
@@ -54,6 +66,19 @@ function Note({ notesData }) {
     console.log(values);
   }
 
+  const initialValues = {
+    title: currentNote.title,
+    description: currentNote.description,
+    category: currentNote.category,
+    isImportant: currentNote.isImportant,
+    isTrashed: currentNote.isTrashed,
+  };
+  const editSchema = yup.object().shape({
+    title: yup.string().required("required"),
+    description: yup.string().required("required"),
+    category: yup.string().required("required"),
+  });
+
   const [openEdit, setOpenEdit] = useState(false);
 
   function handleBack() {
@@ -66,7 +91,7 @@ function Note({ notesData }) {
 
   return (
     <Box display="flex">
-      {openEdit ? (
+      {!openEdit ? (
         <Box width="50%">
           <Header title={"EDIT YOUR NOTE"} />
 
@@ -85,7 +110,118 @@ function Note({ notesData }) {
       ) : (
         <Box width="50%">
           <Header title={"EDIT YOUR NOTE"} />
-          <Formik></Formik>
+          <Formik
+            onSubmit={handleFormSubmit}
+            initialValues={initialValues}
+            validationSchema={editSchema}
+          >
+            {({
+              values,
+              errors,
+              touched,
+              handleBlur,
+              handleChange,
+              handleSubmit,
+            }) => (
+              <form onSubmit={handleSubmit}>
+                <Box
+                  display="flex"
+                  flexDirection="column"
+                  gap="2rem"
+                  width="80%"
+                >
+                  <TextField
+                    variant="filled"
+                    type="text"
+                    label="Title"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.title}
+                    name="title"
+                    error={!!touched.title && !!errors.title}
+                    helperText={touched.title && errors.title}
+                  />
+                  <TextField
+                    variant="filled"
+                    type="text"
+                    label="Description"
+                    onBlur={handleBlur}
+                    onChange={handleChange}
+                    value={values.description}
+                    name="description"
+                    error={!!touched.description && !!errors.description}
+                    helperText={touched.description && errors.description}
+                    multiline
+                    rows={3}
+                  />
+                  <FormControl>
+                    <InputLabel id="select-label">Category</InputLabel>
+                    <Select
+                      labelId="select-label"
+                      label="Category"
+                      fullWidth
+                      variant="filled"
+                      name="category"
+                      value={values.category || "life"}
+                      onChange={handleChange}
+                      onBlur={handleBlur}
+                      error={!!touched.category && !!errors.category}
+                    >
+                      <MenuItem value="shopping">Shopping</MenuItem>
+                      <MenuItem value="life">Life</MenuItem>
+                      <MenuItem value="traveling">Traveling</MenuItem>
+                      <MenuItem value="school">School</MenuItem>
+                      <MenuItem value="work">Work</MenuItem>
+                      <MenuItem value="hobby">Hobby</MenuItem>
+                    </Select>
+                  </FormControl>
+                  <Checkbox
+                    style={{ backgroundColor: "transparent" }}
+                    icon={<FavoriteBorder />}
+                    checkedIcon={<Favorite />}
+                    color="default"
+                    checked={values.isImportant}
+                    onChange={(event) =>
+                      handleChange({
+                        target: {
+                          name: "isImportant",
+                          value: event.target.checked,
+                        },
+                      })
+                    }
+                    onBlur={handleBlur}
+                    name="isImportant"
+                  />
+                </Box>
+                <Box
+                  display="flex"
+                  justifyContent="center"
+                  mt="32px"
+                  width="100%"
+                  gap="20px"
+                  sx={{ transform: "translate(-10%)" }}
+                >
+                  <Button
+                    type="submit"
+                    color="secondary"
+                    variant="contained"
+                    sx={{ width: "20%", padding: "8px 8px" }}
+                  >
+                    Save
+                  </Button>
+                  <Button
+                    type="submit"
+                    color="secondary"
+                    variant="contained"
+                    sx={{ width: "20%", padding: "8px 8px" }}
+                    onClick={() => setOpenEdit(!openEdit)}
+                  >
+                    Cancel
+                  </Button>
+                </Box>
+              </form>
+            )}
+          </Formik>
         </Box>
       )}
       <Box>
